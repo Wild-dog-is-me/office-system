e39966237b981cb9684d3cb137b44987e31b4ac6
zhanghaojian <1427774041@qq.com>
2022-03-21 13:07:14 +0800

    修复分页

---
+++ kwbg-server/src/main/java/com/gqy/server/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.gqy.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ice_water
+ * @Date: 2022/03/21/12:35 PM
+ * @Description: 要做耿沁园的男人
+ */
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
